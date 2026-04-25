da24c429759f65309b446cc87360eac1ef1f41f8
nas7062 <nas7062@naver.com>
2026-04-25 22:04:03 +0900

    chore : lucide-react 추가 및 Terms page 생성

--- src/app/Terms/page.tsx
+++ src/app/Terms/page.tsx
@@ -1,3 +1,12 @@
+import Header from '@/components/ui/Header'
+import { ChevronLeft } from 'lucide-react'
 export default function TermsPage() {
-  return <div>Terms</div>
+  return (
+    <div className="relative w-full">
+      <Header
+        left={<ChevronLeft className=''/>}
+        center={<div className="text-[15px] font-medium text-[#000000]">서비스 이용 동의</div>}
+      />
+    </div>
+  )
 }
