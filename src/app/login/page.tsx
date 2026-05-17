ee8b3f84addf93149aa77d4a0264a782b354f210
nas7062 <nas7062@naver.com>
2026-05-17 21:18:41 +0900

    feat : metadata 설정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,11 @@
+import type { Metadata } from 'next'
 import MainMessage from '@/components/ui/MainMessage'
 import SocialLoginBtns from './_components/SocialLoginBtns'
+
+export const metadata: Metadata = {
+  title: '로그인',
+  description: '소셜 로그인으로 피크다를 시작하세요.',
+}
 
 export default function LoginPage() {
   return (
